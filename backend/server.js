7245cef0e0d96fd83c6f55b84e5332480bc01c62
Ishan Madhawa <ishan@ninodigitalhouse.com>
2021-11-14 20:14:20 +0530

    create routes and models

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,18 @@ const express = require('express');
 //const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+//env config
 require('dotenv').config();
+//Routes imports
+const exercisesRouter = require ('./routes/exercises');
+const userRouter = require('./routes/users');
 
 const app = express();
 const port = process.env.PORT || 5000;
 //creating express server
 app.use(cors());// cors middleware
 app.use(express.json());
-
+//mongo connection
 const uri  = process.env.ATLAS_URI;
 //console.log(uri);
 mongoose.connect(uri);
@@ -20,6 +23,11 @@ connection.once('open',()=>{
 }).on('error',function(error){
     console.log('error is:',error)
 });
+
+//using route
+app.use('/exercises',exercisesRouter);
+app.use('/users',userRouter);
+
 //run server
 app.listen(port,()=>{
     console.log(`Server is running on port: ${port}`);
